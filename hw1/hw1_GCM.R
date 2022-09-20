72f62f99c73e2a84e2007884f1a597209bed2ec7
Chris <u13033663@tuks.co.za>
2022-09-20 09:31:37 -0400

    dawd

--- hw1/hw1_GCM.R
+++ hw1/hw1_GCM.R
@@ -3,6 +3,7 @@
 ########################################
 ##########
 #1
+##########
 # import data and break up into train/validation/test sets
 setwd("E:/GIT_REPOS/Classes/SML/hw1")
 data_df <- read.csv("SML.NN.data.csv")
@@ -212,3 +213,73 @@ knn_lowest_mcr = getMissClassRate(true = valid_df$Y,
 
 print(c(r_lowest_mcr, knn_lowest_mcr))
 
+##########
+#2
+##########
+#####
+#2.1
+#####
+#2.2
+#####
+#2.3
+
+##########
+#3
+##########
+#####
+#3.1
+set.seed(0)
+n=100
+lambda=10
+x=rexp(n,lambda)
+x
+llhd=function(lamb){
+  theta=-n*log(lamb)+lamb*sum(x)
+  return(theta)}
+
+lambda.mle=optimize(f=llhd,interval=c(0.1,20))$minimum
+lambda.mle
+
+#####
+#3.2
+"Take the likelihood function of exponential distribution, and take the natural
+log of it. Then, take the first derivative with respect to lambda, 
+and set it equal to zero. We find the MLE estimator of lambda is n/Sigma x. 
+Take the second derivative and check if it is less than zero to make sure it 
+is the maximum instead of a minimum. "
+lambda.mle2=n/sum(x)
+lambda.mle2
+
+##########
+#4
+##########
+#####
+#4.1
+n=1000;size=4
+CI1=matrix(0,n,2);CI2=matrix(0,n,2);CI3=matrix(0,n,2);coverage=matrix(0,n,3)
+
+width1=rep(0,n);width2=rep(0,n);width3=rep(0,n)
+
+for(j in 1:n){
+  set.seed(j)
+  x=rnorm(size,0,1)
+  CI1[j,]=mean(x)+c(-1,1)*qnorm(0.975)/sqrt(size)
+  CI2[j,]=mean(x)+c(-1,1)*qt(0.975,size-1)*sd(x)/sqrt(size)
+  CI3[j,]=mean(x)+c(-1,1)*qnorm(0.975)*sd(x)/sqrt(size)
+  
+  if(CI1[j,1]<=0 & CI1[j,2]>=0) {coverage[j,1]=1}
+  if(CI2[j,1]<=0 & CI2[j,2]>=0) {coverage[j,2]=1}
+  if(CI3[j,1]<=0 & CI3[j,2]>=0) {coverage[j,3]=1}
+  
+  width1[j]=CI1[j,2]-CI1[j,1]
+  width2[j]=CI2[j,2]-CI2[j,1]
+  width3[j]=CI3[j,2]-CI3[j,1]
+}
+freq.of.coverage=apply(coverage,2,mean)
+freq.of.coverage
+
+#####
+#4.2
+par(mfrow=c(1,2),mar=c(4,4,2,2))
+hist(width2,probability = TRUE, xlab="width", ylab="propotion", main="case2")
+hist(width3,probability = TRUE, xlab="width", ylab="propotion", main="case3")
